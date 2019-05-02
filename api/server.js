75ee0a9aca828a7f499958d4cef46f0bc5180bb0
Pierre Charles <pierre.chls@gmail.com>
2019-05-02 17:20:40 +0200

    :tada: Adding twitter API with node server

---
+++ api/server.js
@@ -0,0 +1,30 @@
+var Twitter = require('./twitter')
+var express = require('express')
+var app = express()
+
+var port = process.env.API_PORT;
+var url = process.env.API_URL;
+var profile = process.env.TWITTER_USERNAME;
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
+})
+
+app.get('/tweets', function (req, res) {
+  var params = {
+    profile: profile,
+    tweet_mode: 'extended',
+    count: 10
+  }
+  Twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
+    res.send(tweets)
+  })
+})
+
+app.listen(port, function () {
+  console.log(`Listening at ${url}:${port}`)
+})
---
+++ api/twitter.js
@@ -0,0 +1,14 @@
+if (process.env.NODE_ENV !== "production") {
+	require('dotenv').config()
+}
+var Twit = require('twit')
+
+var T = new Twit({
+  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+  access_token:         process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  timeout_ms:           60 * 1000
+})
+
+module.exports = T
